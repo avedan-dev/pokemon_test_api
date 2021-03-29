762a827b35c582beab6fa3f898ac9c968ac5fd98
avedan <damir4iki1@gmail.com>
2021-03-29 15:38:02 +0300

    Bug fix

--- deploy/entrypoint.prod.sh
+++ deploy/entrypoint.prod.sh
@@ -7,6 +7,8 @@ then
     done
     echo "PostgreSQL started"
 fi
+python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
+python manage.py collectstatic --no-input
 exec "$@"
